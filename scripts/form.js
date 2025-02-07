05d780e89d936fb5d0d6d394c23017794ccf12cf
haithcock <dakota.haith1997@gmail.com>
2025-02-07 11:18:27 -0500

    created week 05 project

---
+++ scripts/form.js
@@ -0,0 +1,40 @@
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+products.forEach(product => {
+  let productHTML = document.createElement("option");
+  productHTML.setAttribute("value", `${product.name}`);
+  productHTML.innerHTML = product.name;
+  document.getElementById("prodName").appendChild(productHTML);
+});
+
+let numberReviews = Number(window.localStorage.getItem("numberReviews-ls"));
+numberReviews.textContent = `Number of reviews submitted: ${numberReviews}`;
+
+numberReviews++;
+localStorage.setItem("numberReviews-ls", numberReviews);
