ef0d08980380ca0590278f17c4dd27d4f8bba6b2
haithcock <dakota.haith1997@gmail.com>
2025-02-07 11:40:04 -0500

    updated getdates

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,8 @@
+let yr = new Date().getFullYear();
+let lastModified = document.lastModified;
+let author = "🇺🇸 Dakota Raul Hancock 🇺🇸 USA";
+let place = "United States of America";
+
+
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
